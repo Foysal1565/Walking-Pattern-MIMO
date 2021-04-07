e27d03a755171899d0991345755bb6fca967fd3b
Khandaker Foysal Haque <haque1k@cmich.edu>
2021-04-07 13:08:04 -0400

    adding the datagen files

---
+++ prac2.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+print(2>3)
+
+
+# In[9]:
+
+
+print('what is your age?')
+age= input()
+if float(age)>16:
+    print('you are older than 16')
+else:
+        print('you are pretty young')
+
+
+# In[28]:
+
+
+
+for x in range(0,20,3):
+    print(x)
+
+
+# In[34]:
+
+
+#while loop
+loop = True
+
+while loop:
+    age=input('guess my age')
+    if float(age)==27:
+        break
+    
+
+
+# In[ ]:
+
+
+
+
